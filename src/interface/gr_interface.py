5b63f422ed7b5adc4541aef88b06d3028c31ffcb
yachty66 <maxhager28@gmail.com>
2023-10-02 22:04:57 +0200

    gui with 4 players and sample pp

---
+++ src/interface/gr_interface.py
@@ -0,0 +1,55 @@
+#here i am going to create the gradio
+
+#player one
+    #Player Avatars or Icons: Visual representation of each player.
+    #Player Names: Display player names.
+    #Chip Count: Show the number of chips each player has.
+    #Player Status: Indicate if a player is active, folded, all-in, etc.
+    #cards --> https://www.gradio.app/docs/gallery --> markdown
+#gr.Interface(fn=display, inputs="Image", outputs="Image").launch()
+#display(Image(filename='@king.png'))
+
+#player two
+#player three
+#player four
+
+#next round
+
+#cards table
+#https://www.gradio.app/docs/gallery
+
+#small blind and big blind
+
+#pot/bet size
+
+#restart game 
+
+#logging window
+    #contains all spoken words of all participants
+
+
+
+import gradio as gr
+from PIL import Image
+
+def resize_image(image_path, size=(100, 100)):
+    image = Image.open(image_path)
+    return image.resize(size)
+
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            gr.Markdown("# Player 1")
+            gr.Markdown("![Player 4](https://picsum.photos/200/300)")
+        with gr.Column():
+            gr.Markdown("# Player 2")
+            gr.Markdown("![Player 4](https://picsum.photos/200/300)")
+    with gr.Row():
+        with gr.Column():
+            gr.Markdown("![Player 4](https://picsum.photos/200/300)")
+            gr.Markdown("# Player 3")
+        with gr.Column():
+            gr.Markdown("![Player 4](https://picsum.photos/200/300)")
+            gr.Markdown("# Player 4")
+
+demo.launch()
