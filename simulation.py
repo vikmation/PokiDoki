b85ba8bdec2957d1166830f94fbc22c6c5f7103d
yachty66 <maxhager28@gmail.com>
2023-10-11 23:58:21 -0700

    reduced code

--- simulation.py
+++ simulation.py
@@ -1,42 +1,3 @@
-"""
-from engine.game_engine import GameEngine
-from engine.poker_table import PokerTable
-from player.player import Player
-
-# Create poker table and players
-poker_table = PokerTable()
-player1 = Player("Alice", 1000)
-player2 = Player("Bob", 1000)
-
-# Add players to table
-poker_table.players.append(player1)
-poker_table.players.append(player2)
-
-# Create game engine
-game_engine = GameEngine(poker_table)
-
-# Start a round
-game_engine.start_round()
-
-# Print players' hands and community cards
-print(f"Alice's hand: {player1.hand}")
-print(f"Bob's hand: {player2.hand}")
-print(f"Community cards: {poker_table.community_cards}")
----------------------------------------------------------
-agent(
-    path_to_player_config_files=[
-        "player_one.yaml",
-        "player_two.yaml",
-        "player_three.yaml",
-        "player_four.yaml"
-    ],
-    api_key="openai_key",
-    model_name="model_name",
-    table_config="table_config.yaml",
-    logging="true"
-)
-"""
-
 #init players
 
 #create card deck 
@@ -65,6 +26,23 @@ agent(
 import random
 import yaml
 from poker import Suit, Rank, Card, Hand
+import openai
+import logging
+
+"""logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Create a console handler
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+
+# Create a logging format
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(handler)"""
+
 
 class PokerGame:
     def __init__(self, players):
@@ -83,6 +61,7 @@ class PokerGame:
                 self.player_data[player_file] = yaml.safe_load(file)
 
     def start_round(self):
+        #start the round and load data attributes from player and give each player two cards
         self.init_players()
         for player_name, player_attributes in self.player_data.items():
             player_attributes['hand'] = [self.deck.pop(), self.deck.pop()]
@@ -114,16 +93,35 @@ class PokerGame:
             random.shuffle(players)
             self.player_data[players[0]]['small_blind'] = True
             self.player_data[players[1]]['big_blind'] = True
-        print(self.player_data)
+        #print(self.player_data)
 
-    def player_bet(self, next_player):
+    def openai(self, prompt, temperature):
+        openai.api_key = "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"
+        model = "gpt-3.5-turbo"
+        temperature = temperature
+        messages = [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=1000,
+        )
+        result = response.choices[0]['message']['content']
+        return result
+
+    def player_action(self, next_player, round_history, round):
         player_data = self.player_data[next_player]
         player_personality = f"""
-        name: {player_data["name"]}
+        name: {player_data["playerName"]}
         personality description: {player_data["description"]}
-        aggressiveness: {player_data["aggressiveness"]}
-        risk tolerance: {player_data["riskTolerance"]}
-        strategy: {player_data["balanced"]}
+        aggressiveness: {player_data["attributes"]["aggressiveness"]}
+        risk tolerance: {player_data["attributes"]["riskTolerance"]}
+        strategy: {player_data["attributes"]["strategy"]}
         """        
         rounds = [
             "It's pre-flop and there are no community cards on the table.",
@@ -134,53 +132,81 @@ class PokerGame:
         ]
         prompt = f"""
         You are playing a 4 player Texas No-limit Holdem poker game. You have the following personality:
-        
+
         {player_personality}
 
-        {round} and your hand is {player_data["hand"]}. the history of the current round is {round_history}.
-        
+        {rounds[round-1]} and your hand is {player_data["hand"]}. the history of the current round is {round_history}.
+
         Your decisions can be one of fold, raise or limp. Provide your decision without any explanation in the following format:
         DECISION(Raise, Fold, Limp), N BB (if placing a bet, replace N by bet amount)
-        """
-        print(prompt)
-        #parse option and than return
+        """    
+        result = self.openai(prompt, 0.0)    
+        return result
 
     def current_bet(self):
         current_bet = max(player['bet'] for player in self.player_data.values() if 'bet' in player)
         return current_bet
-
-    def betting_round(self):
-        self.set_blinds()
+    
+    def determine_starting_player_index_in_betting_round(self):
         big_blind_player = next((player for player, attributes in self.player_data.items() if attributes.get('big_blind')), None)
         players = list(self.player_data.keys())
         next_player = players[(players.index(big_blind_player) + 1) % len(players)]
-        i = next_player
-        while True:
-            player = self.players[i]
-            if self.player_data['option'] == 'fold':
-                # This player has folded, so they don't get a turn
-                pass
-            elif self.player_data['option'] < self.current_bet:
-                action = self.player_bet(player)
-                if action == 'fold':
-                    self.player_data['option'] = 'fold'
-                elif action == 'call':
-                    self.player_data['bet'] = self.current_bet
-                elif action == 'raise':
-                    #raise is set to value which comes back from current bet
-                    self.player_data['bet'] = action
-                    last_raiser = i
-            else:
-                action = player.get_action(current_bet)
-                if action == 'raise':
-                    raise_amount = player.get_raise_amount(current_bet)
-                    current_bet += raise_amount
-                    player.chips_in_pot = current_bet
-                    last_raiser = i
+        i = players.index(next_player)
+        return i
+    
+    def extract_action(self, action):
+        #openai response is not always clean - need to extract action appropriately
+        pass
+
+    def handle_player_action(self, player, action):
+        if action == 'fold':
+            self.player_data[player]['option'] = 'fold'
+        elif action == 'call':
+            self.player_data[player]['bet'] = self.current_bet
+        elif action == 'raise':
+            self.player_data[player]['bet'] = action  # Assuming action is the raised amount
+            self.current_bet = action
+
+    def skip_folded_player(self, i):
+        if self.player_data[self.players[i]].get('option') == 'fold':
             i = (i + 1) % len(self.players)
-            if i == last_raiser:
-                # We've gotten back to the last player to raise, so the betting round is over
-                break
+        return i
+    
+    def check_end_of_round(self, i):
+        # Check if all players have had a chance to act and all remaining players have bet the same amount
+        if i == len(self.players) and all(player['bet'] == self.current_bet for player in self.player_data.values() if player.get('option') != 'fold'):
+            return True
+        # Check if all but one player have folded
+        if sum(player.get('option') == 'fold' for player in self.player_data.values()) == len(self.players) - 1:
+            return True
+        return False
+    
+    def betting_round(self):
+        #set small and big blinds
+        self.set_blinds()
+        #person after big blind is starting the bet 
+        i = self.determine_starting_player_index_in_betting_round()
+        round_history = []
+        for round in range(1, 6):
+            print("---ROUND {}---".format(round))
+            #each round needs a while loop which runs during the total time of when the betting round is happening 
+            while True:
+                #if player folded already skip to next player
+                i = self.skip_folded_player(i)
+                #let player make an action 
+                action = self.player_action(self.players[i], round_history, round)
+                #function for extracting decision made by player
+                action_extracted = self.extract_action(action)
+                print("action extracted: {}".format(action_extracted))
+                #todo need to add action and in case of raise/call specific amount also
+                round_history.append({self.players[i]: action_extracted})
+
+                self.handle_player_action(self.players[i], action_extracted)
+
+                i = (i + 1) % len(self.players)
+                # Check if all players have had a chance to act and all remaining players have bet the same amount
+                if self.check_end_of_round(i):
+                    break
 
     def deal_flop(self):
         pass 
